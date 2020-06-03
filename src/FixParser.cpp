b336f78edd4115347e53b41ed36043c67a2d848f
Nikolay Zemtsovskiy <zenikolas256@gmail.com>
2020-06-04 00:40:48 +0300

    renaming in FixParser

--- src/FixParser.cpp
+++ src/FixParser.cpp
@@ -29,13 +29,13 @@ namespace fix_parser {
             return res;
         }
 
-        size_t valueLen = pos_field_delimeter - pos_value_delimeter - 1;
+        size_t value_len = pos_field_delimeter - pos_value_delimeter - 1;
 
         constexpr int VALUE_DELIMETER_LEN = 1;
         constexpr int FIELD_DELIMETER_LEN = 1;
 
         std::get<0>(res) = tag;
-        std::get<1>(res) = std::string_view(line.data() + pos_value_delimeter + VALUE_DELIMETER_LEN, valueLen);
+        std::get<1>(res) = std::string_view(line.data() + pos_value_delimeter + VALUE_DELIMETER_LEN, value_len);
         std::get<2>(res) = std::string_view(line.data() + pos_field_delimeter + FIELD_DELIMETER_LEN);
 
         return res;
@@ -44,22 +44,22 @@ namespace fix_parser {
     std::string_view FixParser::parse_group(const std::string_view &groupStr, const std::string_view &numEntriesStr,
                                             const std::string_view &msgType,
                                             const std::string_view &line, uint64_t volumeMultiplier) {
-        auto[numMDEntries, ec] = parseNumber<int>(numEntriesStr);
+        auto[num_md_entries, ec] = parseNumber<int>(numEntriesStr);
         if (ec != std::errc()) {
             std::cerr << "Failed to parse numMDEntries tag value: " << numEntriesStr << std::endl;
             return groupStr;
         }
 
-        std::string_view strLeft = groupStr;
-        for (size_t i = 0; i < numMDEntries; ++i) {
+        std::string_view str_left = groupStr;
+        for (size_t i = 0; i < num_md_entries; ++i) {
             FixMessage msg;
             if (msgType == MARKET_DATA_SNAPSHOT_TYPE) {
                 msg.setUpdateAction("0"); // all msgs in MDEntries group have 'New' MDUpdateAction by default
             }
 
-            while (!msg.isCompleted() && !strLeft.empty()) {
-                auto[tag, value, updatedStr] = getNextTagValue(strLeft);
-                strLeft = updatedStr;
+            while (!msg.isCompleted() && !str_left.empty()) {
+                auto[tag, value, updated_str] = getNextTagValue(str_left);
+                str_left = updated_str;
                 switch (tag) {
                     case MD_ENTRY_TYPE_TAG:
                         msg.setSide(value);
@@ -79,33 +79,33 @@ namespace fix_parser {
             }
 
             if (!msg.isCompleted()) {
-                std::cerr << "Failed to parse " << i << "/" << numMDEntries << " MDEntry group msg in line: " << line
+                std::cerr << "Failed to parse " << i << "/" << num_md_entries << " MDEntry group msg in line: " << line
                           << std::endl;
                 break;
             }
 
             FixMessage::UpdateAction ua = msg.getUpdateAction().value();
-            LevelData levelData;
-            levelData.price = msg.getPrice().value();
-            levelData.qty = msg.getQty().value();
-            levelData.side = msg.getSide().value();
+            LevelData level_data;
+            level_data.price = msg.getPrice().value();
+            level_data.qty = msg.getQty().value();
+            level_data.side = msg.getSide().value();
             if (ua == FixMessage::UpdateAction::Delete) {
-                _order_book.del(levelData);
+                _order_book.del(level_data);
             } else {
-                _order_book.add_change(levelData);
+                _order_book.add_change(level_data);
             }
         }
 
-        return strLeft;
+        return str_left;
     }
 
     void FixParser::handle_line(const std::string &line) {
-        std::string_view lineStr = line.c_str();
-        std::string_view msgType{};
-        uint64_t contractSizeMultiplier{1};
-        while (!lineStr.empty()) {
-            auto[tag, value, strLeft] = getNextTagValue(lineStr);
-            lineStr = strLeft;
+        std::string_view line_str = line.c_str();
+        std::string_view msg_type{};
+        uint64_t contract_size_multiplier{1};
+        while (!line_str.empty()) {
+            auto[tag, value, str_left] = getNextTagValue(line_str);
+            line_str = str_left;
 
             switch (tag) {
                 case MSG_TYPE_TAG: {
@@ -114,7 +114,7 @@ namespace fix_parser {
                         std::cerr << "Skipping unknown message, type tag: " << value << std::endl;
                         return;
                     }
-                    msgType = value;
+                    msg_type = value;
                     break;
                 }
                 case CONTRACT_MULTIPLIER_TAG: {
@@ -123,11 +123,11 @@ namespace fix_parser {
                         std::cerr << "Failed to parse ContractMultiplier tag value: " << value << std::endl;
                         break;
                     }
-                    contractSizeMultiplier = multiplier;
+                    contract_size_multiplier = multiplier;
                     break;
                 }
                 case NO_MD_ENTRIES:
-                    lineStr = parse_group(lineStr, value, msgType, line, contractSizeMultiplier);
+                    line_str = parse_group(line_str, value, msg_type, line, contract_size_multiplier);
                     break;
                 default:
                     break;
